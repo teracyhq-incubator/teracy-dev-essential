7a4e3fc394f1a0952300db87145e2189371fb791
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2018-08-31 16:01:10 +0700

    #1 should implement save mac address feature

--- lib/teracy-dev-essential/config/save_mac_address.rb
+++ lib/teracy-dev-essential/config/save_mac_address.rb
@@ -4,13 +4,45 @@ require 'teracy-dev/plugin'
 module TeracyDevEssential
   module Config
     class SaveMacAddress < TeracyDev::Config::Configurator
-      def configure(settings, config, type:)
-        return
-        case type
-        when 'common'
-          @logger.debug("configure - settings: #{settings}")
-        when 'node'
-          @logger.debug("configure - settings: #{settings}")
+      def configure_node(settings, config)
+        @logger.debug("configure - settings: #{settings}")
+        networks_settings = settings['vm']['networks']
+        hostname = settings['vm']['hostname']
+        node_id = settings['_id']
+
+        configure_networks(node_id, hostname, networks_settings, config)
+      end
+
+      private
+      def configure_networks(node_id, hostname, networks_settings, config)
+        networks_settings ||= []
+        @logger.debug("configure_networks: #{networks_settings}")
+        networks_settings.each do |vm_network|
+          options = {}
+          network_type = vm_network['type'] || vm_network['mode']
+
+          if network_type == 'public_network' and !TeracyDev::Util.exist? vm_network['bridge']
+            network_id = vm_network['_id']
+            id = "#{node_id}-#{network_type}-#{network_id}"
+            options[:id] = id
+
+            mac_file = File.join(TeracyDev::BASE_DIR, '/.vagrant/.' + id + '-public_mac_address')
+            if vm_network['reuse_mac_address']
+              if File.exist?(mac_file)
+                options[:mac] = File.read(mac_file).gsub(/[\s:\n]/,'')
+              else
+                # 08:00:27 is standard prefix for virtualbox
+                mac_address = '08:00:27:' + 3.times.map { '%02x' % rand(0..255) }.join(':')
+                File.write(mac_file, mac_address)
+                options[:mac] = mac_address.gsub(/[\s:\n]/,'')
+              end
+            else
+              if File.exist?(mac_file)
+                FileUtils.rm mac_file
+              end
+            end
+            config.vm.network network_type, options
+          end
         end
       end
     end
