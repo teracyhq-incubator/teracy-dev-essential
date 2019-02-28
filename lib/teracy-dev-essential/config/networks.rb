4e93e8fd9694c00e9e918d7af388f54d9d04435f
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2019-02-28 09:38:35 +0700

    should allow route traffic via public network interface #8

--- lib/teracy-dev-essential/config/networks.rb
+++ lib/teracy-dev-essential/config/networks.rb
@@ -16,6 +16,18 @@ module TeracyDevEssential
       def configure_networks(node_id, networks_settings, config)
         networks_settings ||= []
         @logger.debug("configure_networks: #{networks_settings}")
+
+        public_networks = networks_settings.select{|network| network['type'] == 'public_network'}
+        default_gateway_networks = public_networks.select {|network| network['default_gateway'] == true}
+        allow_default_gateway = true
+
+        # Precheck for this problem https://robertlathanh.com/2009/08/two-subnetworks-on-one-lan-and-linux-arp_filter/
+        if public_networks.length > 1 and default_gateway_networks.length > 0
+          @logger.warn('Ignored default gateway set.')
+          @logger.warn('Use multi public_network and default_gateway at same time could make VM confuse about which interface to connect to internet. Please consider create virtual IP on single public interface instead.')
+          allow_default_gateway = false
+        end
+
         networks_settings.each do |vm_network|
           network_type = vm_network['type'] || vm_network['mode']
           # auto select default bridge for public_network when 'bridge' option is not specified
@@ -28,10 +40,14 @@ module TeracyDevEssential
             @logger.debug("configure_networks: auto select default bridge for #{vm_network} with id: #{id}")
             bridge_interface = get_default_nic()
             @logger.debug("configure_networks: bridge_interface: #{bridge_interface}")
+
+            default_gateway_iface = (allow_default_gateway && vm_network['default_gateway']) || false
+
             if TeracyDev::Util.exist? bridge_interface
               options = {
                 id: id,
-                bridge: bridge_interface
+                bridge: bridge_interface,
+                use_dhcp_assigned_default_route: default_gateway_iface
               }
               config.vm.network network_type, options
               # see:
